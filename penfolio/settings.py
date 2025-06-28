f5f71e950f670565265dd1c18664b85ecb21ca4b
lftobs <tobbytobs1@gmail.com>
2025-06-28 20:01:17 +0100

    chore: deploy

--- penfolio/settings.py
+++ penfolio/settings.py
@@ -53,15 +53,16 @@ LOGGING = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://penfolio-ui.vercel.app/",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "https://penfolio-ui.vercel.app/",
+# ]
 
 
 # (optional) if you need to send cookies or auth headers
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
